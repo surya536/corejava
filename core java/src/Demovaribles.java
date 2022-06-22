9f0eb396b83d070f193f3ab55bb8e04b9da30ea8
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-22 15:28:37 -0400

    while loop

---
+++ core java/src/Demovaribles.java
@@ -0,0 +1,18 @@
+
+public class Demovaribles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int a=10;
+         int b=20;
+         int c = a++;
+         int d = b--;
+         System.out.println("the outpot is," + c);
+         System.out.println("the output is," +d);
+        int  f=a+b;
+         System.out.println ("the output is;" +f);
+      
+         
+	}
+
+}
---
+++ core java/src/control.java
@@ -0,0 +1,21 @@
+
+public class control {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x=4;
+        if(x%2==0)
+        	System.out.println("the value is even");
+        else
+        	System.out.println("the value is odd");
+        int num = 2;
+        int i=0;
+        while(i<10)
+        {
+        	System.out.println("even number,"+num);
+        	num = num+2;
+        	i++;
+        }
+	}
+
+}
