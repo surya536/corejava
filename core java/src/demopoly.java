29e55010f45b7bbe0b154c5b725a8dff694a878f
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-24 16:02:54 -0400

    switch

---
+++ core java/src/demopoly.java
@@ -0,0 +1,26 @@
+
+public class demopoly {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+        switch(x)
+        {
+        case 1:
+        	System.out.println("The value of x is 1");
+        	break;
+        case 2:
+        	System.out.println("The value of x is 2");
+        	break;
+        case 3:
+        	System.out.println("the value of x is 4");
+        	break;
+        default:
+        	System.out.println("the value is 0");
+        	break;
+        }
+     
+        
+	}
+
+}
