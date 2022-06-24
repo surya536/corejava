4906c5eb61dabb5007caa3eeb211a45d4222a2d4
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-24 16:28:56 -0400

    values

---
+++ core java/src/Value.java
@@ -0,0 +1,14 @@
+
+public class Value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int max = 2147483647;
+        int  min= -2147483646;
+        short shortmax = 32767;
+        System.out.println("int max"+max);
+        System.out.println("int min"+min);
+        System.out.println("short "+shortmax);
+	}
+
+}
