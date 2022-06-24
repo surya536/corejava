4c65a9d44046ff2d23d830a60035adacae44b56a
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-24 16:17:04 -0400

    whilelop

---
+++ core java/src/Loop.java
@@ -0,0 +1,14 @@
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i=50;
+       while(i<=100)
+       {
+    	   System.out.println(i);
+    	   i++;
+       }
+	}
+
+}
