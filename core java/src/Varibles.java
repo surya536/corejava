b177db7d3ebc0fcbed713c0b16bfcb6febf74341
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-26 04:29:16 -0400

    varibles

---
+++ core java/src/Varibles.java
@@ -0,0 +1,17 @@
+
+public class Varibles {
+	static int n=100;
+	void method()
+	{
+		int m=90;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int data=10;
+        System.out.println(""+n);
+        System.out.println(""+data);
+        
+	}
+
+}
