a1d596c4207fa31d7f45110cff1788600e7decd7
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-26 04:59:20 -0400

    switch

---
+++ core java/src/Switching.java
@@ -0,0 +1,45 @@
+
+public class Switching {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=3;
+		switch (x) 
+		{
+		case 1: 
+			System.out.println("The value of x is 1");
+			break;
+		case 2:
+			System.out.println("The value of x is 2");
+			break;
+		case 3:
+			System.out.println("The value of x is 3");
+			break;
+		default:
+		System.out.println("The value of x is other than 1,2,3");
+			break;
+
+		}
+		//using string
+		String name = "te";
+
+		switch (name.toLowerCase()) 
+		{
+		case "author":
+			System.out.println("Vikas");
+			break;
+		case "team":
+			System.out.println("Team Java Full Stack");
+			break;
+		case "editor":
+			System.out.println("Vishnu & Krishna");
+			break;
+		default:
+			System.out.println("Invalid entry");
+			break;
+	}
+    //
+
+	}
+
+}
