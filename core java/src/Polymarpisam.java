f683a7b49308d46c3c49738ced4baafaaae12254
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-26 05:15:05 -0400

    type casting

---
+++ core java/src/Polymarpisam.java
@@ -0,0 +1,12 @@
+
+public class Polymarpisam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       int i= 10;  // variable with f is a type of integer which is holding value of 10
+	       long l = (long) i; // defining a var with name l which of datatype long = convert value that is present in I to long
+	       System.out.println(l);
+
+	}
+
+}
