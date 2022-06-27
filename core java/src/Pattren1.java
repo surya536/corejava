dc2320976e0ac7a365b9de130699a8845f45c3fe
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-27 11:15:08 -0400

    patter1

---
+++ core java/src/Pattren1.java
@@ -0,0 +1,21 @@
+
+public class Pattren1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i,j;
+       for ( i=1; i<6; i++) 
+       { 
+           System.out.print("   "); 
+       for (j=2*(6-i); j<=i; j++ )
+       { 
+           System.out.print(" * "); 
+       } 
+
+       System.out.println(); 
+       }
+ 
+ 
+	}
+
+}
---
+++ core java/src/com/svkp/Svkpp.java
@@ -0,0 +1,10 @@
+package com.svkp;
+
+public class Svkpp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
