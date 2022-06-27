813984f864a44f12d23cf2b3a1aa1c30d209f5c2
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-27 17:10:24 -0400

    Final

--- core java/src/com/svkp/Svkpp.java
+++ core java/src/com/svkp/Svkpp.java
@@ -1,8 +1,22 @@
 package com.svkp;
-
-public class Svkpp {
+class Parent
+{
+	final int a = 10;
+	final void display()
+	{
+		System.out.println(" this is parent class");
+	}
+}
+public class Svkpp extends Parent{
+	void display1()
+	{
+		System.out.println("surya");
+	}
 
 	public static void main(String[] args) {
+		Svkpp obj = new Svkpp();
+		obj.display();
+		
 		// TODO Auto-generated method stub
 
 	}
