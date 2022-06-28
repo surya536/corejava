3304d5c670ede2dcbeeac7726a7eec1bc357af10
sri ram <sri ram@DESKTOP-EGH8THE>
2022-06-28 17:14:30 -0400

    scaneer

---
+++ core java/src/com/svkp/Scaneer.java
@@ -0,0 +1,21 @@
+package com.svkp;
+import java.util.Scanner;
+import java.util.stream.*;
+public class Scaneer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("enter the number");
+    Scaneer obj = new Scaneer();
+    Object x = obj.nextInt();
+    System.out.println("the nmber is"+x);
+    
+    
+	}
+
+	private Object nextInt() {
+		// TODO Auto-generated method stub
+		return 66;
+	}
+
+}
--- core java/src/com/svkp/Single.java
+++ core java/src/com/svkp/Single.java
@@ -4,15 +4,6 @@ abstract class Aptraffic_rules
 	abstract void speedlimit();
 	abstract void rashdriving();
 }
-class A 
-{
-	void display()
-	{
-		
-		System.out.println("displayy");
-	}
-	
-}
 public class Single extends Aptraffic_rules {
 	
     void speedlimit()
@@ -25,7 +16,10 @@ public class Single extends Aptraffic_rules {
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        A obj = new A();
+        Single obj = new Single();
+        obj.speedlimit();
+        obj.rashdriving();
+        
         
 	}
 
