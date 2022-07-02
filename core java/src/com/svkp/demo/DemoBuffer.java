8a9e269aa2b5d4acb53659a5cec39416bfc9e320
sri ram <sri ram@DESKTOP-EGH8THE>
2022-07-02 04:44:37 -0400

    arry

--- core java/src/com/svkp/demo/DemoBuffer.java
+++ core java/src/com/svkp/demo/DemoBuffer.java
@@ -1,10 +1,16 @@
 package com.svkp.demo;
+import java.io.BufferedReader;
+import.java.io.InputstreemReader*.;
 
 public class DemoBuffer {
-
+   String name;
+   
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    InputstreemReadr inster=new InputstreemReader("System.in");
+    BufferdReader br = new BufferdReader(br.readline);
+     string s = new Readline();
+     System.out.println("values");
 	}
 
 }
---
+++ core java/src/com/svkp/demo/Demoarry.java
@@ -0,0 +1,19 @@
+package com.svkp.demo;
+
+public class Demoarry {
+
+	public static void main(String[] args) {
+		int d[][] = {{1,2,3,4},{3,4,5},{4,6,7,8,9}};
+		for(int i=0;i<d.length;i++) {
+			for(int j=0;j<=d.length;j++) {
+				System.out.println(" "+d[i][j]);
+			}
+			System.out.println("");
+		}
+		
+		
+		
+		
+	}
+
+}
