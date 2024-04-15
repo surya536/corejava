84fa15c0d8c1abd7092e850c4eef92de8f197f1b
surya536 <61462092+surya536@users.noreply.github.com>
2024-04-15 12:36:36 +0530

    Update Loop.java

--- core java/src/Loop.java
+++ core java/src/Loop.java
@@ -10,5 +10,16 @@ public class Loop {
     	   i++;
        }
 	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int i=50;
+       while(i<=100)
+       {
+    	   System.out.println(i);
+    	   i++;
+       }
+	}
+
 
 }
